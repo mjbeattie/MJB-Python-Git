6d9ba3f92b2aafd8fd4ac2a5dc6f629fd711291b
Matthew Beattie <mjbeattie@ou.edu>
2022-10-14 16:45:20 -0700

    test merge

--- scratchpad.py
+++ scratchpad.py
@@ -1,4 +1,13 @@
 def main():
-    for i in range(0,5):
-        print(i)
+    userName = input('Enter your full name:')
+    userAge = '101'
+    str = '0123456789'
+    while (int(userAge)<=0 or int(userAge)>=100):
+        userAge = input('Enter your age as an integer between 1 and 99:')
+        for i in userAge:
+            if i not in str:
+                print('That was not an integer, try again...')
+                userAge = '101'
+    intUserAge = int(userAge)
+    print('Your age is:', intUserAge)
 main()
